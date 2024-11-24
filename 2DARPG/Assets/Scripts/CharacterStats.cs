23d22e2a83638d734c92d107a1fab3b582bdb399
ADuserNEEDsupport <43333418+ADuserNEEDsupport@users.noreply.github.com>
2024-11-24 23:47:36 +0800

    角色数据统计2
    封装所有属性字段，增加列表方法，方便后续装备或道具增加对应属性计算

--- 2DARPG/Assets/Scripts/CharacterStats.cs
+++ 2DARPG/Assets/Scripts/CharacterStats.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    public int damage;
-    public int maxHealth;
+    public Stat damage;
+    public Stat maxHealth;
 
     [SerializeField] private int currentHealth;
 
     void Start()
     {
-        currentHealth = maxHealth;
+        currentHealth = maxHealth.GetValue();
+
+        damage.AddModifiers(4);
     }
 
     public void TakeDamage(int _damage)
