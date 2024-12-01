999d5874fe90e1d3387b0fa94b4de37ff013c2aa
ADuserNEEDsupport <43333418+ADuserNEEDsupport@users.noreply.github.com>
2024-12-01 22:42:28 +0800

    角色统计数据5
    闪避与护甲

--- 2DARPG/Assets/Scripts/CharacterStats.cs
+++ 2DARPG/Assets/Scripts/CharacterStats.cs
@@ -1,13 +1,20 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
+    [Header("Major stats")]
     public Stat strength;
-    public Stat damage;
+    public Stat agility;
+    public Stat intelligence;
+    public Stat vitality;
+
+    [Header("Defensive stats")]
     public Stat maxHealth;
+    public Stat armor;
+    public Stat evasion;
+
+
+    public Stat damage;
 
     [SerializeField] private int currentHealth;
 
@@ -18,10 +25,15 @@ public class CharacterStats : MonoBehaviour
 
     public virtual void DoDamage(CharacterStats _targetStats)
     {
+        if (TargetCanAvoidAttack(_targetStats))
+            return;
+
         int totalDamage = damage.GetValue() + strength.GetValue();
 
-        _targetStats.TakeDamage(totalDamage);
+        totalDamage -= _targetStats.armor.GetValue();
+        totalDamage = CheckTargetArmor(_targetStats, totalDamage);
     }
+
 
     public virtual void TakeDamage(int _damage)
     {
@@ -36,5 +48,22 @@ public class CharacterStats : MonoBehaviour
     protected virtual void Die()
     {
         //throw new NotImplementedException();
+    }
+    private bool TargetCanAvoidAttack(CharacterStats _targetStats)
+    {
+        int totalEavasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+
+        if (Random.Range(0, 100) < totalEavasion)
+        {
+            return true;
+        }
+
+        return false;
+    }
+    private int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
+    {
+        totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);
+        _targetStats.TakeDamage(totalDamage);
+        return totalDamage;
     }
 }
